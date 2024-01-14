de3eda5c0be777ff29fda64c4ce72635c8d44619
RolandReff <Henrik.reffs@gmail.com>
2024-01-14 13:04:23 +0100

    Init, with all the api calls that needs to be done from domeneshop

---
+++ domainnameshop-template.sh
@@ -0,0 +1,26 @@
+### All API commands used in this script can be found within the offical domeneshop API documentation
+### that can be found bellow, but short explanations can be found within the script.
+### https://api.domeneshop.no/docs/
+
+### To create the credentials visit the page bellow
+### https://domene.shop/admin?view=api
+token=""
+secret=""
+domain=""
+
+
+IFS='.' read -r subdomain SecTopDomain <<< "$domain"
+
+
+id="1680019"
+
+IPv6_regex=(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))
+
+### Gets the json info about the top domain
+curl https://$token:$secret@api.domeneshop.no/v0/domains?domain=$SecTopDomain
+
+### Gets the json info abut the subdomain
+curl https://$token:$secret@api.domeneshop.no/v0/domains/$id/dns?host=$subdomain
+
+### Updates the DNS record of the provided domain 
+curl https://$token:$secret@api.domeneshop.no/v0/dyndns/update?hostname=$domain
