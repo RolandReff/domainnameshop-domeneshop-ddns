ccc862db04e6266f32510a746cf1787512d4e247
RolandReff <Henrik.reffs@gmail.com>
2024-01-23 20:57:57 +0000

    Cleaned up code, started adding regex's for IPv4 and IPv6 IP validation.

--- domainnameshop-template.sh
+++ domainnameshop-template.sh
@@ -10,36 +10,30 @@ domain=""
 
 IFS='.' read -r subdomain SecTopDomain <<< "$domain"
 ipv6_regex="(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
-
-ipv6TEST="2a01:799:19eb:9300:6573:2132:8575:ecf7";
-
-
-### Checks for the current public IPv6
-#current_ip=$(curl -s -6 https://api64.ipify.org || curl -s -6 https://ipv6.icanhazip.com)
-
-#if [[ ! $current_ip =~ ^$ipv6_regex$ ]]; then
-#    echo "Failed to validate the IP address"
-#    exit 1
-#fi
+ipv4_regex='"id":"\K\[^"]*'
 
 idRegex='"id":"\K\d+';
-ipv4regex='^ip=(\d.*)'
-### '\"id":(\d*)'
+
 
 ### Gets the json info about the top domain and gets the domain ID
-idRegex='\"id":(\d*)';
 testID=$( curl https://$token:$secret@api.domeneshop.no/v0/domains?domain=$SecTopDomain); ret=$?
+
 id=$(echo "$testID" | grep -oP '"id":\K\d+')
+echo $id
 
 ### Gets the json info abut the subdomain
-echo $(curl https://$token:$secret@api.domeneshop.no/v0/domains/$id/dns?host=$subdomain);
+old_ip=$(curl https://$token:$secret@api.domeneshop.no/v0/domains/$id/dns?host=$subdomain);
+echo $old_ip
+old_ip=$(echo "$old_ip" | grep -oP '"id":"\K[^"]*')
+echo $old_ip
 
-### Updates the DNS record of the provided domain 
-if [[ $old_ip != $current_ip ]] then
-    #curl https://$token:$secret@api.domeneshop.no/v0/dyndns/update?hostname=$domain
+current_ip=$(curl -g -4 ifconfig.me)
+echo $current_ip
+
+if [[ $old_ip != $current_ip ]]; then
+#    curl https://$token:$secret@api.domeneshop.no/v0/dyndns/update?hostname=$domain
     echo "They are NOT the same"
 else
-    echo "they are the same"
+   echo "they are the same"
 fi
-
-cat > hell.txt
+cat > helllo.txt
